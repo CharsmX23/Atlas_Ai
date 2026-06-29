65e263dccfa98862da5e1db38d80db4e28763110
CharsmX23 <charsmello23@gmail.com>
2026-06-29 19:05:32 +0530

    fix vercel eslint build error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
-module.exports = nextConfig
+  images: { unoptimized: true },
+};
+
+module.exports = nextConfig;
